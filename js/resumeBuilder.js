6d2f9344fff21964e5c2eee0b2f635e1eaa1a09c
Abhinav Thukral <abhinav@abhinavthukral.com>
2015-06-30 13:42:49 +0530

    Added all data

---
+++ js/resumeBuilder.js
@@ -0,0 +1,73 @@
+var bio = {
+    "name": "Abhinav Thukral",
+    "role": "Web Developer",
+    "pictureURL": "images/me.jpg",
+    "welcomeMessage": "Hi, I am a Web Developer based out of Ottawa",
+    "contacts":{
+        "mobile": "613-555-5555",
+        "email": "abhinav@abhinavthukral.com",
+        "github": "abhinavthukral28",
+        "twitter": "thukralabhinav",
+        "location": "Ottawa, Canada"
+    },
+    "skills": ["programming", "JavaScript", "HTML5", "CSS", "Responsive Design"]
+};
+
+var work = {
+    "jobs": [
+        {
+            "employer": "Espial Group Inc",
+            "title": "Software Developer Coop",
+            "location": "Ottawa",
+            "dates": "January 2014 to August 2014",
+            "description": "• Customized ‘Jasmine’ JavaScript Unit Testing Framework to as per the needs of the company leading to automation of testing process of JavaScript Middleware - Extended the functionality of company’s JavaScript Middleware API - Wrote programs in Java to automate the process of download of simulation data from the internet"
+        },
+        {
+            "employer": "Bright Land School",
+            "title": "Systems Administrator",
+            "location": "Amritsar",
+            "dated": "July 2013 to August 2013, May 2015 to July 2015",
+            "description": "Installed a Linux based Virtual Private Server to host school’s growing web services - Applied HTML and CSS skills to redesign the school website (In Progress) - Trained the staff members to use the new school learning management software"
+        }
+    ]
+};
+
+var education = {
+    "schools": [
+        {
+            "name": "Carleton University",
+            "location": "Ottawa",
+            "degree": "Bachelor of Computer Science",
+            "majors": [
+                "Computer Science"
+            ],
+            "dates": "September 2011 to Present",
+            "url": "http://carleton.ca"
+        }
+    ],
+    "onlineCourses": [
+        {
+            "title": "Front End D eveloper Nanodegree",
+            "school": "Udacity",
+            "dates": "May 2015 to Present",
+            "url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
+        }
+    ]
+};
+
+var projects = [
+    {
+        "title": "Homey - Winner of Best Intel Edison App at U of T Hacks 2015",
+        "dates": "January 29, 2015 to February 1, 2015",
+        "description": " Designed a Home Monitoring System using Intel Edison Platform - Used JavaSctipt collect data from various sensors - Designed a companion Pebble Smart Watch App that received the data from sensors using AJAX - Effectively used NodeJS with Express Framework to transmit the sensor data to the companion Android and Pebble app",
+        "images": []
+    },
+    {
+        "title": "CarletonRobotics.com",
+        "dates": "September 2013 to August 2014",
+        "description": "Developed Website for Carleton University Robotics Club",
+        "images": []
+    }
+];
+
+
